866a499ec88dfad6e85651a1b9258fa9d22cac22
papeiron <151983680+papeiron@users.noreply.github.com>
2025-02-09 11:28:53 +0300

    ADD: maintence

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ const nextConfig = {
   images: {
     domains: ["cwsxpgwvvnfzugcsrvyv.supabase.co"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "query",
+            key: "maintenance",
+            value: "true",
+          },
+        ],
+        destination: "/maintenance",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Maintence.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Maintence = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+      <div className="max-w-md w-full space-y-8 bg-white p-6 rounded-xl shadow-lg">
+        <div className="text-center">
+          <h2 className="mt-6 text-3xl font-bold text-gray-900">Coming Soon...</h2>
+
+          <div className="mt-8 animate-pulse">
+            <div className="h-2 bg-gray-200 rounded-full max-w-md mx-auto"></div>
+            <div className="space-y-3 mt-4">
+              <div className="h-2 bg-gray-200 rounded-full"></div>
+              <div className="h-2 bg-gray-200 rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maintence;
